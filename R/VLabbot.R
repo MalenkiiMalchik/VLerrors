98c10dc3cc0f7fd39e14ae84ebfb0d3b78251910
MalenkiiMalchik <ajr166@georgetown.edu>
2023-03-02 15:02:06 -0500

    Added packages for packaging

--- R/VLabbot.R
+++ R/VLabbot.R
@@ -2,197 +2,229 @@
 #'
 #' Reads Abbott files, cleans data, and uploads them to Google Drive. Optionally returns one of two processed datasets.
 #'
-#' @param return Specify whether to return a dataset. Valid arguments are 'errors' and 'percent'
+#' @param return Specify whether to return a dataset. Valid arguments are 'errors' and 'percent'. Defaults to FALSE
+#' @param download Specify whether to download fresh files from Google Drive. Defaults to FALSE
+#' @param data Specify where to download files to or where they are stored
 #' @importFrom magrittr %>%
 #' @export
 
-VLabbot <- function(return){
-
+VLabbot <- function(return = FALSE,
+                    download = FALSE,
+                    data) {
   #### LOAD DATA ============================================================================
 
+  if (download == TRUE) {
+    googledrive::drive_auth()
 
-  drive_auth()
+    folder <- "198YgVgnr9_z6YhqbQWbDlo0GJK70u_cV"
 
-  folder <- "198YgVgnr9_z6YhqbQWbDlo0GJK70u_cV"
+    glamr::import_drivefile(
+      drive_folder = folder,
+      filename = "test_activity_abbott.csv",
+      folderpath = data,
+      zip = FALSE
+    )
 
-  files_in_folder <- googledrive::drive_ls(googledrive::as_id(folder))
+    glamr::import_drivefile(
+      drive_folder = folder,
+      filename = "test_rate.csv",
+      folderpath = data,
+      zip = FALSE
+    )
 
-  glamr::import_drivefile(drive_folder = folder,
-                          filename = "test_activity_abbott.csv",
-                          folderpath = data,
-                          zip = FALSE)
+    glamr::import_drivefile(
+      drive_folder = folder,
+      filename = "test.csv",
+      folderpath = data,
+      zip = FALSE
+    )
 
-  glamr::import_drivefile(drive_folder = folder,
-                          filename = "test_rate.csv",
-                          folderpath = data,
-                          zip = FALSE)
+    glamr::import_drivefile(
+      drive_folder = folder,
+      filename = "instrument_master.csv",
+      folderpath = data,
+      zip = FALSE
+    )
+    glamr::import_drivefile(
+      drive_folder = folder,
+      filename = "error.csv",
+      folderpath = data,
+      zip = FALSE
+    )
 
-  glamr::import_drivefile(drive_folder = folder,
-                          filename = "test.csv",
-                          folderpath = data,
-                          zip = FALSE)
+    glamr::import_drivefile(
+      drive_folder = folder,
+      filename = "error_type.csv",
+      folderpath = data,
+      zip = FALSE
+    )
+    glamr::import_drivefile(
+      drive_folder = folder,
+      filename = "site.csv",
+      folderpath = data,
+      zip = FALSE
+    )
 
-  glamr::import_drivefile(drive_folder = folder,
-                          filename = "instrument_master.csv",
-                          folderpath = data,
-                          zip = FALSE)
-  glamr::import_drivefile(drive_folder = folder,
-                          filename = "error.csv",
-                          folderpath = data,
-                          zip = FALSE)
+    glamr::import_drivefile(
+      drive_folder = folder,
+      filename = "test_type.csv",
+      folderpath = data,
+      zip = FALSE
+    )
+  }
 
-  glamr::import_drivefile(drive_folder = folder,
-                          filename = "error_type.csv",
-                          folderpath = data,
-                          zip = FALSE)
-  glamr::import_drivefile(drive_folder = folder,
-                          filename = "site.csv",
-                          folderpath = data,
-                          zip = FALSE)
-  glamr::import_drivefile(drive_folder = folder,
-                          filename = "test_type.csv",
-                          folderpath = data,
-                          zip = FALSE)
+  test_activity_abbott <-
+    readr::read_csv(paste0(data, "/test_activity_abbott.csv"))
 
+  test_rate <- readr::read_csv(paste0(data, "/test_rate.csv"))
 
-  test_activity_abbott <- read_csv(here("Data", "test_activity_abbott.csv"))
+  test = readr::read_csv(paste0(data, "/test.csv"))
 
-  test_rate <- read_csv(here("Data", "test_rate.csv"))
+  instrument_master = readr::read_csv(paste0(data, "/instrument_master.csv"))
 
-  test = read_csv(here("Data", "test.csv"))
+  error = readr::read_csv(paste0(data, "/error.csv"))
 
-  instrument_master = read_csv(here("Data", "instrument_master.csv"))
+  error_type = readr::read_csv(paste0(data, "/error_type.csv"))
 
-  error = read_csv(here("Data", "error.csv"))
+  site = readr::read_csv(paste0(data, "/site.csv"))
 
-  error_type = read_csv(here("Data", "error_type.csv"))
-
-  site = read_csv(here("Data", "site.csv"))
-
-  test_type = read_csv(here("Data", "test_type.csv"))
+  test_type = readr::read_csv(paste0(data, "/test_type.csv"))
 
 
   #### DATA WRANGLING ============================================================================
 
   instrument_master = instrument_master %>%
-    select(instrument_id, instrument_model, supplier_site_id) %>%
-    filter(instrument_id %in% unique(test_activity_abbott$instrument_id)) %>%
-    mutate(instrument_model = toupper(instrument_model))
+    dplyr::select(instrument_id, instrument_model, supplier_site_id) %>%
+    dplyr::filter(instrument_id %in% unique(test_activity_abbott$instrument_id)) %>%
+    dplyr::mutate(instrument_model = toupper(instrument_model))
 
   test_activity_abbott = test_activity_abbott %>%
-    left_join(instrument_master, by = c("instrument_id" = "instrument_id"))
+    dplyr::left_join(instrument_master, by = c("instrument_id" = "instrument_id"))
 
   test_activity_abbott = test_activity_abbott %>%
-    mutate(instrument_model = str_remove_all(instrument_model, "®"))
+    dplyr::mutate(instrument_model = str_remove_all(instrument_model, "®"))
 
   error_type = error_type %>%
-    select(error_type_id, error_type_name, error_type_description)
+    dplyr::select(error_type_id, error_type_name, error_type_description)
 
   site = site %>%
-    select(supplier_site_id, supplier_site_name)
+    dplyr::select(supplier_site_id, supplier_site_name)
 
   test_type = test_type %>%
-    select(test_type_id, test_type_psm)
+    dplyr::select(test_type_id, test_type_psm)
 
   error_codes = test_activity_abbott %>%
-    left_join(test_type) %>%
-    filter(test_type_psm %in% c("EID", "EID/VL", "VL")) %>%
-    mutate(well_number = str_extract(test_record_id, "\\|\\d+$")) %>%
-    mutate(well_number = str_remove_all(well_number, "\\|")) %>%
-    mutate(test_record_id = str_remove(test_record_id, "\\|\\d+$")) %>%
-    filter(!is.na(error_type_id)) %>%
-    mutate(error_type_id = as.character(error_type_id)) %>%
-    left_join(error_type, by = c("error_type_id" = "error_type_id")) %>%
-    left_join(site, by = c("supplier_site_id" = "supplier_site_id")) %>%
-    select(instrument_id,
-           instrument_model,
-           supplier_site_id,
-           supplier_site_name,
-           country_code,
-           well_number,
-           test_type_id,
-           test_type_psm,
-           error_type_id,
-           error_type_name,
-           error_type_description,
-           error_count,
-           test_date,
-           is_patient_test,
-           is_control_test,
-           patient_test_error_flag,
-           control_test_error_flag,
-           excluded_tests
+    dplyr::left_join(test_type) %>%
+    dplyr::filter(test_type_psm %in% c("EID", "EID/VL", "VL")) %>%
+    dplyr::mutate(well_number = stringr::str_extract(test_record_id, "\\|\\d+$")) %>%
+    dplyr::mutate(well_number = stringr::str_remove_all(well_number, "\\|")) %>%
+    dplyr::mutate(test_record_id = stringr::str_remove(test_record_id, "\\|\\d+$")) %>%
+    dplyr::filter(!is.na(error_type_id)) %>%
+    dplyr::mutate(error_type_id = as.character(error_type_id)) %>%
+    dplyr::left_join(error_type, by = c("error_type_id" = "error_type_id")) %>%
+    dplyr::left_join(site, by = c("supplier_site_id" = "supplier_site_id")) %>%
+    dplyr::select(
+      instrument_id,
+      instrument_model,
+      supplier_site_id,
+      supplier_site_name,
+      country_code,
+      well_number,
+      test_type_id,
+      test_type_psm,
+      error_type_id,
+      error_type_name,
+      error_type_description,
+      error_count,
+      test_date,
+      is_patient_test,
+      is_control_test,
+      patient_test_error_flag,
+      control_test_error_flag,
+      excluded_tests
     ) %>%
-    mutate(test_date = as.character(test_date)) %>%
-    mutate(test_month_year = format(ymd(test_date), "%m-%Y"))
+    dplyr::mutate(test_date = as.character(test_date)) %>%
+    dplyr::mutate(test_month_year = format(lubridate::ymd(test_date), "%m-%Y"))
 
-  if(return != "percent"){
-    while(tries<3){
-      test = try(googledrive::drive_upload(media = error_codes,
-                                           name = paste0("error_codes_", Sys.Date(), ".csv"),
-                                           path = as_id("1PZGmTQ0fG4_zafM3JfTNL_ek-JeFifvw"),
-                                           overwrite = TRUE))
-      if(class(test)=="try-error"){
-        tries = tries+1
-        } else {break}
+  if (return != "percent") {
+    while (tries < 3) {
+      test = try(googledrive::drive_upload(
+        media = error_codes,
+        name = paste0("error_codes_", Sys.Date(), ".csv"),
+        path = googledrive::as_id("1PZGmTQ0fG4_zafM3JfTNL_ek-JeFifvw"),
+        overwrite = TRUE
+      ))
+      if (class(test) == "try-error") {
+        tries = tries + 1
+      } else {
+        break
+      }
     }
   }
-  if(return == "errors"){
+  if (return == "errors") {
     return(error_codes)
   }
 
   error_codes_percentages = test_activity_abbott %>%
-    mutate(well_number = str_extract(test_record_id, "\\|\\d+$")) %>%
-    mutate(well_number = str_remove_all(well_number, "\\|")) %>%
-    mutate(test_record_id = str_remove(test_record_id, "\\|\\d+$")) %>%
-    mutate(error_type_id = as.character(error_type_id)) %>%
-    left_join(error_type, by = c("error_type_id" = "error_type_id")) %>%
-    left_join(site, by = c("supplier_site_id" = "supplier_site_id"))
+    dplyr::mutate(well_number = stringr::str_extract(test_record_id, "\\|\\d+$")) %>%
+    dplyr::mutate(well_number = stringr::str_remove_all(well_number, "\\|")) %>%
+    dplyr::mutate(test_record_id = stringr::str_remove(test_record_id, "\\|\\d+$")) %>%
+    dplyr::mutate(error_type_id = as.character(error_type_id)) %>%
+    dplyr::left_join(error_type, by = c("error_type_id" = "error_type_id")) %>%
+    dplyr::left_join(site, by = c("supplier_site_id" = "supplier_site_id"))
 
   # Obtain percent of plates affected by each code per month-year
 
   error_codes_errors = error_codes_percentages %>%
-    mutate(test_date = as.character(test_date)) %>%
-    mutate(test_month_year = format(ymd(test_date), "%m-%Y")) %>%
-    filter(error_count>0) %>%
-    group_by(test_record_id,
-             test_month_year,
-             instrument_model,
-             supplier_site_name,
-             country_code,
-             error_type_id) %>%
-    summarize(errors = n()) %>%
-    ungroup() %>%
-    group_by(test_month_year,
-             error_type_id,
-             instrument_model,
-             supplier_site_name,
-             country_code) %>%
-    summarize(plates_affected = n())
+    dplyr::mutate(test_date = as.character(test_date)) %>%
+    dplyr::mutate(test_month_year = format(lubridate::ymd(test_date), "%m-%Y")) %>%
+    dplyr::filter(error_count > 0) %>%
+    dplyr::group_by(
+      test_record_id,
+      test_month_year,
+      instrument_model,
+      supplier_site_name,
+      country_code,
+      error_type_id
+    ) %>%
+    dplyr::summarize(errors = n()) %>%
+    dplyr::ungroup() %>%
+    dplyr::group_by(
+      test_month_year,
+      error_type_id,
+      instrument_model,
+      supplier_site_name,
+      country_code
+    ) %>%
+    dplyr::summarize(plates_affected = n())
 
   error_codes_plates = error_codes_percentages %>%
-    mutate(test_date = as.character(test_date)) %>%
-    mutate(test_month_year = format(ymd(test_date), "%m-%Y")) %>%
-    filter(!duplicated(test_record_id)) %>%
-    group_by(test_month_year) %>%
-    summarize(plates = n())
+    dplyr::mutate(test_date = as.character(test_date)) %>%
+    dplyr::mutate(test_month_year = format(lubridate::ymd(test_date), "%m-%Y")) %>%
+    dplyr::filter(!duplicated(test_record_id)) %>%
+    dplyr::group_by(test_month_year) %>%
+    dplyr::summarize(plates = n())
 
   abbott_error_percentages = error_codes_errors %>%
-    left_join(error_codes_plates) %>%
-    mutate(percentage = plates_affected*100/plates)
+    dplyr::left_join(error_codes_plates) %>%
+    dplyr::mutate(percentage = plates_affected * 100 / plates)
 
-  if(return != "errors"){
-    while(tries<3){
-      test = try(googledrive::drive_upload(media = abbott_error_percentages,
-                                           name = paste0("abbott_error_percentages_", Sys.Date(), ".csv"),
-                                           path = as_id("1PZGmTQ0fG4_zafM3JfTNL_ek-JeFifvw"),
-                                           overwrite = TRUE))
-      if(class(test)=="try-error"){
-        tries = tries+1
-        } else {break}
+  if (return != "errors") {
+    while (tries < 3) {
+      test = try(googledrive::drive_upload(
+        media = abbott_error_percentages,
+        name = paste0("abbott_error_percentages_", Sys.Date(), ".csv"),
+        path = googledrive::as_id("1PZGmTQ0fG4_zafM3JfTNL_ek-JeFifvw"),
+        overwrite = TRUE
+      ))
+      if (class(test) == "try-error") {
+        tries = tries + 1
+      } else {
+        break
+      }
     }
   }
-  if(return == "percent"){
+  if (return == "percent") {
     return(abbott_error_percentages)
   }
 
--- R/VLroche.R
+++ R/VLroche.R
@@ -3,10 +3,16 @@
 #' Reads Roche files, cleans data, and uploads them to Google Drive. Optionally returns processed dataset.
 #'
 #' @param return Specify whether to return a dataset. Defaults to FALSE
+#' @param download Specify whether to download fresh files from Google Drive. Defaults to FALSE
+#' @param data Specify where to download files to or where they are stored
 #' @importFrom magrittr %>%
 #' @export
 
-drive_auth()
+VLroche <- function(return = FALSE, download = FALSE, data){
+
+  if(download == TRUE){
+
+googledrive::drive_auth()
 
 folder <- "198YgVgnr9_z6YhqbQWbDlo0GJK70u_cV"
 
@@ -44,60 +50,60 @@ glamr::import_drivefile(drive_folder = folder,
                         filename = "site.csv",
                         folderpath = data,
                         zip = FALSE)
+}
 
+test_activity_roche <- readr::read_csv(paste0(data, "/test_activity_roche.csv"))
 
-test_activity_roche <- read_csv(here("Data", "test_activity_roche.csv"))
+test_rate <- readr::read_csv(paste0(data, "/test_rate.csv"))
 
-test_rate <- read_csv(here("Data", "test_rate.csv"))
+test = readr::read_csv(paste0(data, "/test.csv"))
 
-test = read_csv(here("Data", "test.csv"))
+instrument_master = readr::read_csv(paste0(data, "/instrument_master.csv"))
 
-instrument_master = read_csv(here("Data", "instrument_master.csv"))
+error = readr::read_csv(paste0(data, "/error.csv"))
 
-error = read_csv(here("Data", "error.csv"))
+error_type = readr::read_csv(paste0(data, "/error_type.csv"))
 
-error_type = read_csv(here("Data", "error_type.csv"))
-
-site = read_csv(here("Data", "site.csv"))
+site = readr::read_csv(paste0(data, "/site.csv"))
 
 
 #### DATA WRANGLING ============================================================================
 
 instrument_master = instrument_master %>%
-  select(instrument_id, instrument_model, supplier_site_id) %>%
-  mutate(instrument_id = as.numeric(instrument_id)) %>%
-  filter(instrument_id %in% unique(test_activity_roche$instrument_id)) %>%
-  mutate(instrument_model = toupper(instrument_model))
+  dplyr::select(instrument_id, instrument_model, supplier_site_id) %>%
+  dplyr::mutate(instrument_id = as.numeric(instrument_id)) %>%
+  dplyr::filter(instrument_id %in% unique(test_activity_roche$instrument_id)) %>%
+  dplyr::mutate(instrument_model = toupper(instrument_model))
 
 test_activity_roche = test_activity_roche %>%
-  left_join(instrument_master, by = c("instrument_id" = "instrument_id"))
+  dplyr::left_join(instrument_master, by = c("instrument_id" = "instrument_id"))
 
 test_activity_roche = test_activity_roche %>%
-  filter(str_detect(instrument_model, "00")) %>%
-  mutate(instrument_model = str_remove_all(instrument_model, "®"))
+  dplyr::filter(stringr::str_detect(instrument_model, "00")) %>%
+  dplyr::mutate(instrument_model = stringr::str_remove_all(instrument_model, "®"))
 
 
 error_codes = test_activity_roche %>%
-  mutate(internal_id = paste0("INTERNAL-",row_number())) %>%
-  separate(error_code_list, c("a","b","c","d","e","f","g","h", "i"), sep = ",") %>%
-  pivot_longer(cols = c("a","b","c","d","e","f","g","h", "i"), values_to = "error_code_list")
+  dplyr::mutate(internal_id = paste0("INTERNAL-",row_number())) %>%
+  tidyr::separate(error_code_list, c("a","b","c","d","e","f","g","h", "i"), sep = ",") %>%
+  tidyr::pivot_longer(cols = c("a","b","c","d","e","f","g","h", "i"), values_to = "error_code_list")
 
 error_codes = error_codes %>%
-  filter(name == "a" | !is.na(error_code_list))
+  dplyr::filter(name == "a" | !is.na(error_code_list))
 
 
 error_type = error_type %>%
-  filter(supplier_id == 1000 &
+  dplyr::filter(supplier_id == 1000 &
            error_type_id %in% error_codes$error_code_list) %>%
-  select(error_type_id, error_type_description)
+  dplyr::select(error_type_id, error_type_description)
 
 site = site %>%
-  select(supplier_site_id, supplier_site_name)
+  dplyr::select(supplier_site_id, supplier_site_name)
 
 roche_errors = error_codes %>%
-  left_join(error_type, by = c("error_code_list" = "error_type_id")) %>%
-  left_join(site, by = c("supplier_site_id" = "supplier_site_id")) %>%
-  select(internal_id,
+  dplyr::left_join(error_type, by = c("error_code_list" = "error_type_id")) %>%
+  dplyr::left_join(site, by = c("supplier_site_id" = "supplier_site_id")) %>%
+  dplyr::select(internal_id,
          name,
          error_code_list,
          error_type_description,
@@ -113,24 +119,24 @@ roche_errors = error_codes %>%
          patient_test_error_flag,
          control_test_error_flag,
          excluded_test) %>%
-  mutate(month = floor_date(test_date, unit = "month")) %>%
-  mutate(test_month_year = zoo::as.yearmon(month)) %>%
-  select(-month) %>%
-  mutate(id_error = paste0(internal_id, "-", error_code_list)) %>%
-  mutate(dups = duplicated(id_error))
+  dplyr::mutate(month = lubridate::floor_date(test_date, unit = "month")) %>%
+  dplyr::mutate(test_month_year = zoo::as.yearmon(month)) %>%
+  dplyr::select(-month) %>%
+  dplyr::mutate(id_error = paste0(internal_id, "-", error_code_list)) %>%
+  dplyr::mutate(dups = duplicated(id_error))
 
 # Filter out the duplicate errors
 
 roche_errors = roche_errors %>%
-  filter(dups == F) %>%
-  select(-id_error, -dups)
+  dplyr::filter(dups == F) %>%
+  dplyr::select(-id_error, -dups)
 
 roche_errors$error_type_description[roche_errors$error_code_list=="6282.36"]<-"Pipetted volume was not sufficient. Sample was not transferred"
 
 while(tries<3){
   test = try(googledrive::drive_upload(media = roche_errors,
                                        name = paste0("roche_errors_", Sys.Date(), ".csv"),
-                                       path = as_id("1PZGmTQ0fG4_zafM3JfTNL_ek-JeFifvw"),
+                                       path = googledrive::as_id("1PZGmTQ0fG4_zafM3JfTNL_ek-JeFifvw"),
                                        overwrite = TRUE))
   if(class(test)=="try-error"){
     tries = tries+1
@@ -139,4 +145,6 @@ while(tries<3){
 
 if(return == TRUE){
   return(roche_errors)
+}
+
 }
